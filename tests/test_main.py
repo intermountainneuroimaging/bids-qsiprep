6ac2c9d5e0ce3d5dcda03327d713963bf31a0b12
Pablo Velasco <pablovelasco@flywheel.io>
2022-04-22 09:58:39 -0400

    Finish unit tests for main.run

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,6 @@
 """Module to test main.py"""
+import logging
+import os.path
 from pathlib import Path
 from unittest.mock import MagicMock
 
@@ -113,21 +115,45 @@ def test_prepare(mocked_gear_options):
     assert warnings == expected_warnings
 
 
-def test_run(tmpdir, mocked_gear_options):
+# Test 2x2 use cases:
+# - dry_run = True/False
+@pytest.mark.parametrize("dry_run", [True, False])
+# - main_command = "echo" / "ohce"  ; this checks what happens if exec_command returns an error
+@pytest.mark.parametrize("main_command", ["echo", "ohce"])
+def test_run(tmpdir, caplog, mocked_gear_options, dry_run, main_command):
     """Unit tests for run"""
 
-    main.generate_command = MagicMock()
-    main.get_and_log_environment = MagicMock()
+    logging.getLogger(__name__)
+    caplog.set_level(logging.INFO)
+
+    my_cmd = [main_command, "Foo"]
+    main.generate_command = MagicMock(return_value=my_cmd)
+    main.get_and_log_environment = MagicMock(return_value={})
 
     # main.run attempts to create the "destination-id" folder, so need to modify the default one:
     foo_gear_options = mocked_gear_options
-    foo_gear_options["destination-id"] = tmpdir / foo_gear_options["destination-id"]
+    foo_gear_options["output-dir"] = tmpdir / foo_gear_options["output-dir"]
+    if dry_run:
+        foo_gear_options["dry-run"] = True
 
-    exit_code = main.run(mocked_gear_options, {})
+    if main_command == "ohce" and not dry_run:
+        with pytest.raises(RuntimeError):
+            exit_code = main.run(mocked_gear_options, {})
+            assert exit_code > 0
+        return
+    else:
+        exit_code = main.run(mocked_gear_options, {})
 
-    main.generate_command.assert_called_once()
-
-    # TO-DO: - finish tests for main.run
-    #        - write test for main.run with dry-run
-    #        - write test for case main.run gives an error
+    # The following only runs for the cases in which errors are not expected:
     assert exit_code == 0
+    main.generate_command.assert_called_once()
+    main.get_and_log_environment.assert_called_once()
+    assert os.path.exists(
+        Path(foo_gear_options["output-dir"]) / Path(foo_gear_options["destination-id"])
+    )
+    # Check that there is a record in the log saying "Executing command" followed by my_cmd.
+    # This shows that "exec_command" was run with the expected command.
+    executing_command_lines = [
+        l.message for l in caplog.records if "Executing command" in l.message
+    ]
+    assert [" ".join(my_cmd) in l for l in executing_command_lines]
