ed7c7edf2dbbb81a3a0f758baf5abf46a67bdc16
Pablo Velasco <pablovelasco@flywheel.io>
2022-05-04 13:40:01 -0400

    ENH: conftest.py: Add `install_gear_results`, to use in future tests.

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,10 +2,13 @@
 Set up parameters for testing. Picked up by pytest automatically.
 """
 
+import shutil
+from pathlib import Path
 from unittest.mock import MagicMock
 
 import pytest
 from flywheel_gear_toolkit import GearToolkitContext
+from flywheel_gear_toolkit.utils.zip_tools import unzip_archive
 
 
 @pytest.fixture
@@ -34,3 +37,47 @@ def mocked_context(mocked_gear_options):
         client="",
         destination={"id": mocked_gear_options["destination-id"]},
     )
+
+
+FWV0 = Path.cwd()
+
+
+@pytest.fixture
+def install_gear_results():
+    def _method(zip_name, gear_output_dir=None):
+        """Un-archive gear results to simulate running inside a real gear.
+
+        This will delete and then install: config.json input/ output/ work/ freesurfer/
+
+        Args:
+            zip_name (str): name of zip file that holds simulated gear.
+            gear_output_dir (str): where to install the contents of the zipped file
+        """
+
+        # location of the zip file:
+        gear_tests = Path("/src/tests/data/")
+        if not gear_tests.exists():  # fix for running in circleci
+            gear_tests = FWV0 / "tests" / "data/"
+
+        # where to install the data
+        if not gear_output_dir or not Path(gear_output_dir).exists():
+            gear_output_dir = FWV0
+
+        print("\nRemoving previous gear...")
+
+        if Path(gear_output_dir / "config.json").exists():
+            Path(gear_output_dir / "config.json").unlink()
+
+        for dir_name in ["input", "output", "work", "freesurfer"]:
+            path = Path(gear_output_dir / dir_name)
+            if path.exists():
+                print(f"shutil.rmtree({str(path)}")
+                shutil.rmtree(path)
+
+        print(f'\ninstalling new gear, "{zip_name}"...')
+        unzip_archive(gear_tests / zip_name, str(gear_output_dir))
+
+        # The "freesurfer" directory needs to have the standard freesurfer
+        # "subjects" directory and "license.txt" file.
+
+    return _method
